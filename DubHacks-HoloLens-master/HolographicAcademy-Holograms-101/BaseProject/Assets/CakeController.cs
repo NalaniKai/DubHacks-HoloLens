b9382470869715dc9c371ef24136191845941c1c
Nalani-Chun <chunm18@up.edu>
2016-10-16 07:56:11 -0700

    baker audio file added

---
+++ DubHacks-HoloLens-master/HolographicAcademy-Holograms-101/BaseProject/Assets/CakeController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CakeController : MonoBehaviour {
+
+
+    GameObject myWaffle;
+    GameObject myChef;
+
+    // Use this for initialization
+    void Start () {
+        //look through heirarchy for gameobject called waffle
+        myWaffle = GameObject.Find("Waffle");
+        myChef = GameObject.Find("Charakter_Chef");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnSelect()
+    {
+        Debug.Log("hi");
+
+        foreach (Renderer r in myWaffle.transform.GetComponentsInChildren<Renderer>())
+        {
+            r.enabled = true;
+        }
+        foreach (Renderer r in myChef.transform.GetComponentsInChildren<Renderer>())
+        {
+            r.enabled = true;
+        }
+    }
+}
---
+++ DubHacks-HoloLens-master/HolographicAcademy-Holograms-101/BaseProject/Assets/ChefController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChefController : MonoBehaviour {
+    AudioSource audioSource = null;
+    AudioClip baker = null;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.spatialize = true;
+        audioSource.spatialBlend = 1.0f;
+        audioSource.dopplerLevel = 0.0f;
+        audioSource.rolloffMode = AudioRolloffMode.Custom;
+        foreach (Renderer r in gameObject.transform.GetComponentsInChildren<Renderer>())
+        {
+            r.enabled = false;
+        }
+        baker = Resources.Load<AudioClip>("test");
+        audioSource.clip = baker;
+        audioSource.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
